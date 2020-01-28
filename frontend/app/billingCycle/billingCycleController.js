1bd31183ca4815b61424f858048d322f52e27f71
Letícia Naomi Takeshiro <leticiatakeshiro@hotmail.com>
2020-01-28 14:36:32 -0300

    Listar ciclos de pagamento

--- frontend/app/billingCycle/billingCycleController.js
+++ frontend/app/billingCycle/billingCycleController.js
@@ -7,14 +7,24 @@
 
     function BillingCycleController($http, msgs) {
         const vm = this
-        vm.create = function() {
-            const url = 'http://localhost:3003/api/billingCycles'
-            $http.post(url, vm.billingCycle).then(function(response) {
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.refresh = function() {
+            $http.get(url).then(function(response) {
                 vm.billingCycle = {}
+                vm.billingCycles = response.data
+            })
+        }
+
+        vm.create = function() {
+            $http.post(url, vm.billingCycle).then(function(response) {
+                vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!!')
             }).catch(function(response) {
                 msgs.addError(response.data.errors)
             })
         }
+
+        vm.refresh()
     }      
 })()
